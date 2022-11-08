e898edd60baeaa00c1affab3697646445670da43
Benjamin Keene <39920749+benkeene@users.noreply.github.com>
2022-11-08 18:02:51 -0500

    Update problem5_4.py

--- problem5_4.py
+++ problem5_4.py
@@ -24,84 +24,83 @@ from sklearn.svm import SVC
 import numpy as np
 
 
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(5, 5))
 plt.subplots_adjust(bottom=0.05, top=0.9, left=0.05, right=0.95)
 
-X1, Y1 = make_moons(100)
+X1, Y1 = make_moons(100, noise=0.1)
 cmap = plt.get_cmap('Spectral', 3)
-
-
-
 
 
 plt.subplot(221)
 
-clf = SVC(kernel='linear', C = 1.0)
+clf = SVC(kernel='linear', C=1.0)
 
 clf.fit(X1, Y1)
 
 x2, y2 = np.meshgrid(np.linspace(X1[:, 0].min()-.5, X1[:, 0].max()+.5, 20),
-                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20) )
+                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20))
 
 pred = clf.predict(np.c_[x2.ravel(), y2.ravel()])
 
 
+plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10,
+            cmap=cmap, label='Linear prediction on grid')
 
-plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10, cmap=cmap, label='Linear prediction on grid')
+plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap,
+            ec='black', label='Given values')
 
-plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap, ec='black', label='Given values')
-
-plt.contourf(x2, y2, pred.reshape(x2.shape), cmap=cmap, alpha=0.6, levels=2, zorder=0)
+plt.contourf(x2, y2, pred.reshape(x2.shape),
+             cmap=cmap, alpha=0.6, levels=2, zorder=0)
 
 #plt.legend(ncol=2, loc="lower center", bbox_to_anchor=(0.5,1.01))
 
 plt.title("linear, C = 1.0")
 
 
-
-
-
-
-
 plt.subplot(222)
 
-clf = SVC(kernel='rbf', C = 0.0001, gamma=0.0001)
+clf = SVC(kernel='rbf', C=0.0001, gamma=0.0001)
 
 clf.fit(X1, Y1)
 
 x2, y2 = np.meshgrid(np.linspace(X1[:, 0].min()-.5, X1[:, 0].max()+.5, 20),
-                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20) )
+                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20))
 
 pred = clf.predict(np.c_[x2.ravel(), y2.ravel()])
 
-plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10, cmap=cmap, label='rbf prediction on grid')
+plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10,
+            cmap=cmap, label='rbf prediction on grid')
 
-plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap, ec='black', label='Given values')
+plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap,
+            ec='black', label='Given values')
 
-plt.contourf(x2, y2, pred.reshape(x2.shape), cmap=cmap, alpha=0.6, levels=2, zorder=0)
+plt.contourf(x2, y2, pred.reshape(x2.shape),
+             cmap=cmap, alpha=0.6, levels=2, zorder=0)
 
 #plt.legend(ncol=2, loc="lower center", bbox_to_anchor=(0.5,1.01))
 
 plt.title("rbf, C = 0.0001, gamma = 0.0001, underfit")
 
 
-
 plt.subplot(223)
 
-clf = SVC(kernel='rbf', C = 1, gamma=1)
+clf = SVC(kernel='rbf', C=1, gamma=1)
 
 clf.fit(X1, Y1)
 
 x2, y2 = np.meshgrid(np.linspace(X1[:, 0].min()-.5, X1[:, 0].max()+.5, 20),
-                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20) )
+                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20))
 
 pred = clf.predict(np.c_[x2.ravel(), y2.ravel()])
 
-plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10, cmap=cmap, label='rbf prediction on grid')
+plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10,
+            cmap=cmap, label='rbf prediction on grid')
 
-plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap, ec='black', label='Given values')
+plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap,
+            ec='black', label='Given values')
 
-plt.contourf(x2, y2, pred.reshape(x2.shape), cmap=cmap, alpha=0.6, levels=2, zorder=0)
+plt.contourf(x2, y2, pred.reshape(x2.shape),
+             cmap=cmap, alpha=0.6, levels=2, zorder=0)
 
 #plt.legend(ncol=2, loc="lower center", bbox_to_anchor=(0.5,1.01))
 
@@ -110,20 +109,23 @@ plt.title("rbf, C = 1, gamma = 1")
 
 plt.subplot(224)
 
-clf = SVC(kernel='rbf', C = 1000, gamma=1000)
+clf = SVC(kernel='rbf', C=1000, gamma=1000)
 
 clf.fit(X1, Y1)
 
 x2, y2 = np.meshgrid(np.linspace(X1[:, 0].min()-.5, X1[:, 0].max()+.5, 20),
-                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20) )
+                     np.linspace(X1[:, 1].min()-.5, X1[:, 1].max()+.5, 20))
 
 pred = clf.predict(np.c_[x2.ravel(), y2.ravel()])
 
-plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10, cmap=cmap, label='rbf prediction on grid')
+plt.scatter(x2.ravel(), y2.ravel(), c=pred, s=10,
+            cmap=cmap, label='rbf prediction on grid')
 
-plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap, ec='black', label='Given values')
+plt.scatter(X1[:, 0], X1[:, 1], c=Y1, s=50, cmap=cmap,
+            ec='black', label='Given values')
 
-plt.contourf(x2, y2, pred.reshape(x2.shape), cmap=cmap, alpha=0.6, levels=2, zorder=0)
+plt.contourf(x2, y2, pred.reshape(x2.shape),
+             cmap=cmap, alpha=0.6, levels=2, zorder=0)
 
 #plt.legend(ncol=2, loc="lower center", bbox_to_anchor=(0.5,1.01))
 
